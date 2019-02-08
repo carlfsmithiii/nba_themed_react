7f87ec1352902cf5b55a4c61848e1f79cb5e5a33
Carl Smith <cfsmithiii@gmail.com>
2019-02-08 15:21:41 -0500

    template router and  hoc for header/nav

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import App from "./components/App";
+import Routes from "./Routes";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
